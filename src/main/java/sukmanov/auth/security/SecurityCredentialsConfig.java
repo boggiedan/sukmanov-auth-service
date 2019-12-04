62ced226ec5861bd00fff1f1c4e80ef8d67a8b3e
boggie <sukmanov.b@gmail.com>
2019-12-04 23:21:36 +0100

    initial setup

---
+++ src/main/java/sukmanov/auth/security/SecurityCredentialsConfig.java
@@ -0,0 +1,7 @@
+package sukmanov.auth.security;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+public class SecurityCredentialsConfig {
+}
---
+++ src/test/java/sukmanov/auth/SukmanovAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package sukmanov.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SukmanovAuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
