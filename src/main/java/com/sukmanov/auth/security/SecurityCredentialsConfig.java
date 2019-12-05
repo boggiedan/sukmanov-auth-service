294d3a816e37faaaf1c39df220dec38ac95ff768
boggie <sukmanov.b@gmail.com>
2019-12-06 00:53:22 +0100

    authentification added

--- src/main/java/com/sukmanov/auth/security/SecurityCredentialsConfig.java
+++
@@ -1,7 +0,0 @@
-package com.sukmanov.auth.security;
-
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
-@EnableWebSecurity
-public class SecurityCredentialsConfig {
-}
